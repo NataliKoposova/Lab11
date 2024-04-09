6d7773f9c484c661f8067af42a188c6561595d6e
Koposova1 <Koposova1@KOPOSOVANA>
2024-04-09 13:28:53 +0300

    Добавьте файлы проекта.

---
+++ Lab11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввведите первый коэффициент");
+            double k=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ввведите второй коэффициент");
+            double b=Convert.ToDouble(Console.ReadLine());
+
+            UR ur=new UR(k,b);
+            Console.WriteLine(ur.Root());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab11/UR.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    internal class UR
+    {//0=kx+b
+        double k;
+        double b;
+
+
+        public UR(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            double x = (-b) / k;
+            if (x == 0 || k <= 0)
+                return "Решений нет";
+            double r1 = (-b) / k;
+            return $"X={r1}";
+        }
+    }
+}
